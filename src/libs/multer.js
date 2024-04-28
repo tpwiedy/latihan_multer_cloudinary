0b3c9f19b4cd5d2d7485e0a034fca42e89877f14
tpwiedy <wiedy.pratama@gmail.com>
2024-04-28 17:06:03 +0700

    change uploads path

--- src/libs/multer.js
+++ src/libs/multer.js
@@ -2,22 +2,21 @@ const multer = require('multer');
 
 const localStorage = multer.diskStorage({
   destination: function (req, file, callback) {
-    callback(null, './uploads');
+    callback(null, '../../uploads');
   },
   filename: function (req, file, callback) {
     callback(null, file.originalname);
   },
 });
 
-const fileFilter = (req, file, callback) => {
-  if (['image/png', 'image/jpg', 'image/jpeg'].includes(file.mimetype)) {
-    callback(null, true);
+const fileFilter = function (req, file, callback) {
+  if (['image/png', 'image/jpeg', 'image/jpg'].includes(file.mimetype)) {
+    return callback(null, true);
   }
-  return callback(
-    new Error('Only .png, .jpg and .jpeg format allowed!'),
-    false
-  );
+
+  return callback(new Error('File type must be .jpeg, .jpg or .png'));
 };
+
 const uploader = multer({ storage: localStorage, fileFilter });
 
 module.exports = uploader;
